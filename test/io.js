8598c66a0794051b929c01b0b27f2f8498a81613
amigcamel <amigcamel@gmail.com>
2018-07-10 00:52:42 +0800

    add readFrom: support for reading from path or stream

---
+++ test/io.js
@@ -0,0 +1,23 @@
+const stream = require('stream');
+const io = require('../lib/io.js');
+
+describe('Read from file', () => {
+  it('type should be string', (done) => {
+    io.readFrom('test/test.html', (output) => {
+      (typeof output).should.be.type('string');
+      done();
+    });
+  });
+});
+
+describe('Read from stream', () => {
+  it('type should be string', (done) => {
+    const s = new stream.Readable();
+    s.push('<html></html>');
+    s.push(null);
+    io.readFrom('test/test.html', (output) => {
+      (typeof output).should.be.type('string');
+      done();
+    });
+  });
+});
