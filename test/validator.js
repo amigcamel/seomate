81eba9fc92a7beef06f2fbbf78f5cbb808359b88
amigcamel <amigcamel@gmail.com>
2018-07-09 17:39:21 +0800

    rewrite in class

--- test/validator.js
+++ test/validator.js
@@ -1,9 +1,10 @@
 const should = require('should'); // eslint-disable-line no-unused-vars
-const v = require('../lib/validator');
+const Validator = require('../lib/validator');
 
 describe('validate h1', () => {
   it('should return 1', (done) => {
-    v.validate('<h1></h1>').should.equal(1);
+    const v = new Validator('<h1></h1>');
+    v.checkH1().should.equal(1);
     done();
   });
 });
