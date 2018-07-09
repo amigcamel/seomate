93cf41722b1c0a253234c8472b617f99713bc626
amigcamel <amigcamel@gmail.com>
2018-07-09 17:46:47 +0800

    ensure cheerio is using htmlparser2

--- test/validator.js
+++ test/validator.js
@@ -1,6 +1,14 @@
 const should = require('should'); // eslint-disable-line no-unused-vars
 const Validator = require('../lib/validator');
 
+describe('Cheerio use htmlparser2', () => {
+  it('should not return undefined', (done) => {
+    const v = new Validator('<h1></h1>');
+    v.dom('h1').get(0).startIndex.should.be.type('number');
+    done();
+  });
+});
+
 describe('validate h1', () => {
   it('should return 1', (done) => {
     const v = new Validator('<h1></h1>');
