fb37633e83a38716ad20908d33a1e6ed0b9f7e50
amigcamel <amigcamel@gmail.com>
2018-07-10 12:42:25 +0800

    return promise for io.readFrom

--- test/io.js
+++ test/io.js
@@ -3,10 +3,12 @@ const io = require('../lib/io.js');
 
 describe('Read from file', () => {
   it('type should be string', (done) => {
-    io.readFrom('test/test.html', (output) => {
-      (typeof output).should.be.type('string');
-      done();
-    });
+    io.readFrom('test/test.html').then(
+      (output) => {
+        (typeof output).should.be.type('string');
+        done();
+      },
+    );
   });
 });
 
@@ -15,9 +17,11 @@ describe('Read from stream', () => {
     const s = new stream.Readable();
     s.push('<html></html>');
     s.push(null);
-    io.readFrom('test/test.html', (output) => {
-      (typeof output).should.be.type('string');
-      done();
-    });
+    io.readFrom(s).then(
+      (output) => {
+        (typeof output).should.be.type('string');
+        done();
+      },
+    );
   });
 });
